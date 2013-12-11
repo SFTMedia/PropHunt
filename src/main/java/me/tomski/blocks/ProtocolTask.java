fdf937a8a11fc3604eeb9805774c37e0c7d199f6
TomSki <tomaszwojciechowski@hotmail.co.uk>
2013-12-11 13:10:52 +0000

    Bye bye left/right clicks

--- src/main/java/me/tomski/blocks/ProtocolTask.java
+++ src/main/java/me/tomski/blocks/ProtocolTask.java
@@ -22,113 +22,119 @@ public class ProtocolTask implements Listener {
 
 
     public void initProtocol() {
-        PropHunt.protocolManager.getAsynchronousManager().registerAsyncHandler(new PacketAdapter(plugin, PacketType.Play.Client.BLOCK_PLACE) {
+        PropHunt.protocolManager.getAsynchronousManager().
 
-            @Override
-            public void onPacketSending(PacketEvent event) {
-                System.out.println("sent packet " + event.getPacketType());
-            }
+                registerAsyncHandler(new PacketAdapter(plugin, PacketType.Play.Client.BLOCK_PLACE) {
 
-            @Override
-            public void onPacketReceiving(PacketEvent event) {
-                if (GameManager.hiders.contains(event.getPlayer().getName())) {
-                    int x = event.getPacket().getIntegers().read(0);
-                    int y = event.getPacket().getIntegers().read(1);
-                    int z = event.getPacket().getIntegers().read(2);
-                    for (SolidBlock s : SolidBlockTracker.solidBlocks.values()) {
-                        if (s.loc.getBlockX() == x) {
-                            if (s.loc.getBlockY() == y) {
-                                if (s.loc.getBlockZ() == z) {
-                                    event.setCancelled(true);
+                    @Override
+                    public void onPacketSending(PacketEvent event) {
+                        System.out.println("sent packet " + event.getPacketType());
+                    }
+
+                    @Override
+                    public void onPacketReceiving(PacketEvent event) {
+                        if (GameManager.hiders.contains(event.getPlayer().getName())) {
+                            int x = event.getPacket().getIntegers().read(0);
+                            int y = event.getPacket().getIntegers().read(1);
+                            int z = event.getPacket().getIntegers().read(2);
+                            for (SolidBlock s : SolidBlockTracker.solidBlocks.values()) {
+                                if (s.loc.getBlockX() == x) {
+                                    if (s.loc.getBlockY() == y) {
+                                        if (s.loc.getBlockZ() == z) {
+                                            event.setCancelled(true);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (GameManager.seekers.contains(event.getPlayer().getName())) {
+                            int x = event.getPacket().getIntegers().read(0);
+                            int y = event.getPacket().getIntegers().read(1);
+                            int z = event.getPacket().getIntegers().read(2);
+                            for (SolidBlock s : SolidBlockTracker.solidBlocks.values()) {
+                                if (s.loc.getBlockX() == x) {
+                                    if (s.loc.getBlockY() == y) {
+                                        if (s.loc.getBlockZ() == z) {
+                                            event.setCancelled(true);
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
+
+                    // Get entity trackers is not thread safe
                 }
-                if (GameManager.seekers.contains(event.getPlayer().getName())) {
-                    int x = event.getPacket().getIntegers().read(0);
-                    int y = event.getPacket().getIntegers().read(1);
-                    int z = event.getPacket().getIntegers().read(2);
-                    for (SolidBlock s : SolidBlockTracker.solidBlocks.values()) {
-                        if (s.loc.getBlockX() == x) {
-                            if (s.loc.getBlockY() == y) {
-                                if (s.loc.getBlockZ() == z) {
-                                    event.setCancelled(true);
+
+                ).
+
+                syncStart();
+
+
+        PropHunt.protocolManager.getAsynchronousManager().
+
+                registerAsyncHandler(new PacketAdapter(plugin, PacketType.Play.Client.ARM_ANIMATION) {
+
+                    @Override
+                    public void onPacketSending(PacketEvent event) {
+                        System.out.println("sent packet " + event.getPacketType());
+                    }
+
+                    @Override
+                    public void onPacketReceiving(PacketEvent event) {
+                        final int ATTACK_REACH = 3;
+
+                        Player observer = event.getPlayer();
+                        Location observerPos = observer.getEyeLocation();
+                        Vector3D observerDir = new Vector3D(observerPos.getDirection());
+
+                        Vector3D observerStart = new Vector3D(observerPos);
+                        Vector3D observerEnd = observerStart.add(observerDir.multiply(ATTACK_REACH));
+
+                        Player hit = null;
+
+                        // Get nearby entities
+                        try {
+                            for (Player target : PropHunt.protocolManager.getEntityTrackers(observer)) {
+                                // No need to simulate an attack if the
+                                // player is already visible
+                                if (!observer.canSee(target)) {
+                                    // Bounding box of the given player
+                                    Vector3D targetPos = new Vector3D(target.getLocation());
+                                    Vector3D minimum = targetPos.add(-0.5, 0, -0.5);
+                                    Vector3D maximum = targetPos.add(0.5, 1.67, 0.5);
+
+                                    if (hasIntersection(observerStart,
+                                            observerEnd, minimum, maximum)) {
+                                        if (hit == null
+                                                || hit.getLocation().distanceSquared(observerPos) > target.getLocation().distanceSquared(observerPos)) {
+                                            hit = target;
+                                        }
+                                    }
                                 }
                             }
+                        } catch (FieldAccessException ex) {
+                            // Player is dead
                         }
-                    }
-                }
-            }
 
-            // Get entity trackers is not thread safe
-        }).syncStart();
-
-
-        PropHunt.protocolManager.getAsynchronousManager().registerAsyncHandler(new PacketAdapter(plugin, PacketType.Play.Client.ARM_ANIMATION) {
-
-            @Override
-            public void onPacketSending(PacketEvent event) {
-                System.out.println("sent packet " + event.getPacketType());
-            }
-
-            @Override
-            public void onPacketReceiving(PacketEvent event) {
-                final int ATTACK_REACH = 3;
-
-                Player observer = event.getPlayer();
-                Location observerPos = observer.getEyeLocation();
-                Vector3D observerDir = new Vector3D(observerPos.getDirection());
-
-                Vector3D observerStart = new Vector3D(observerPos);
-                Vector3D observerEnd = observerStart.add(observerDir.multiply(ATTACK_REACH));
-
-                Player hit = null;
-
-                // Get nearby entities
-                try {
-                    for (Player target : PropHunt.protocolManager.getEntityTrackers(observer)) {
-                        // No need to simulate an attack if the
-                        // player is already visible
-                        if (!observer.canSee(target)) {
-                            // Bounding box of the given player
-                            Vector3D targetPos = new Vector3D(target.getLocation());
-                            Vector3D minimum = targetPos.add(-0.5, 0, -0.5);
-                            Vector3D maximum = targetPos.add(0.5, 1.67, 0.5);
-
-                            if (hasIntersection(observerStart,
-                                    observerEnd, minimum, maximum)) {
-                                if (hit == null
-                                        || hit.getLocation().distanceSquared(observerPos) > target.getLocation().distanceSquared(observerPos)) {
-                                    hit = target;
-                                }
+                        // Simulate a hit against the closest player
+                        if (hit != null) {
+                            PacketContainer useEntity = PropHunt.protocolManager.createPacket(PacketType.Play.Client.USE_ENTITY);
+                            useEntity.getIntegers()
+                                    .write(0, hit.getEntityId());
+                            try {
+                                PropHunt.protocolManager.recieveClientPacket(event.getPlayer(), useEntity);
+                            } catch (Exception e) {
+                                e.printStackTrace();
                             }
+
                         }
                     }
-                } catch (FieldAccessException ex) {
-                    // Player is dead
+
+                    // Get entity trackers is not thread safe
                 }
 
-                // Simulate a hit against the closest player
-                if (hit != null) {
-                    PacketContainer useEntity = PropHunt.protocolManager.createPacket(PacketType.Play.Client.USE_ENTITY);
-                    useEntity.getIntegers()
-                            .write(0, hit.getEntityId())
-                            .write(1, 0 /* LEFT_CLICK */);
-
-                    try {
-                        PropHunt.protocolManager.recieveClientPacket(event.getPlayer(), useEntity);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            }
-
-            // Get entity trackers is not thread safe
-        }
-
-        ).
+                ).
 
                 syncStart();
     }
